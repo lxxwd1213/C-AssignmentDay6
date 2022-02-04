df46aded1627109bcf25429488784b041a598012
Laura Li <lxxwd1213@gmail.com>
2022-02-04 00:00:18 -0600

    Create Program.cs

---
+++ Exercise03/Exercise03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Exercise03 {
+
+    class program {
+
+        public static void Main(String[] args) {
+
+            for (int i = 1; i <= 100; i++)
+            {
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzzis");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzziss");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
