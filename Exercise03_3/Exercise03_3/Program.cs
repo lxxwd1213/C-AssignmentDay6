732a52f8fb00f0e3356df2c4e9598cff0d139ee3
Laura Li <lxxwd1213@gmail.com>
2022-02-04 00:00:50 -0600

    Create Program.cs

---
+++ Exercise03_3/Exercise03_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise03_3 {
+
+    class program {
+
+        public static void Main(String[] args) {
+
+            int correctNumber = new Random().Next(3) + 1;
+            Console.WriteLine("Please enter your guess number:");
+            int guessedNumber = int.Parse(Console.ReadLine());
+
+            if (guessedNumber == correctNumber) {
+                Console.WriteLine("Correct!");
+            } else if (guessedNumber < correctNumber) {
+                Console.WriteLine("Low!");
+            } else if (guessedNumber > correctNumber)
+            {
+                Console.WriteLine("High!");
+            } else if (guessedNumber > 2147483647) {
+                Console.WriteLine("Invaid Number.");
+            } 
+        }
+    }
+}
+
