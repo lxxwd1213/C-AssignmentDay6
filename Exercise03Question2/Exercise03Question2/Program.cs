0d7dcb40bbd4d2c0d98b0e45b9ad812565498cf6
Laura Li <lxxwd1213@gmail.com>
2022-02-04 00:01:20 -0600

    Create Program.cs

---
+++ Exercise03Question2/Exercise03Question2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise03Question2 {
+    class program {
+        public static void Main(String[] args)
+        {
+
+            Console.WriteLine("Pyramid.");
+
+            for (int i = 1; i < 5; i++)
+            {
+
+                for (int j = 3; j >= i; j--)
+                {
+                    Console.Write(" ");
+                }
+                int k = 0;
+                while (k != 2 * i - 1)
+                { 
+                    Console.Write("*");
+                    k++;
+                }
+                Console.WriteLine();//Enter
+            }
+
+        }
+
+    }
+}
